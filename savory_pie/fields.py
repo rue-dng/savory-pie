035a7cef7d66a262366405c9b91b8cbfe2dc1f5b
Jason Michalski <jmichalski@ruelala.com>
2013-02-08 11:56:19 -0500

    Add FKPropertyField

--- savory_pie/fields.py
+++ savory_pie/fields.py
@@ -31,3 +31,51 @@ class PropertyField(object):
 
     def prepare(self, queryset):
         return queryset
+
+
+class FKPropertyField(object):
+    """
+    Used to flatten fields of related models in to a single API object.
+
+    FKPropertyField('other.name', type=int) will return this json {'name': other.name}
+    """
+    def __init__(self, property=None, json_property=None, type=None):
+        self.property = property
+        self.json_property = json_property or _python_to_js_name(self.property.split('.')[-1])
+        self.type = type
+
+    def _get_property(self, obj):
+        property = obj
+        for segment in self.property.split('.'):
+            property = getattr(property, segment)
+        return property
+
+    def _set_property(self, obj, value):
+        property = obj
+        segments = self.property.split('.')
+        for segment in segments[:-1]:
+            property = getattr(property, segment)
+        setattr(property, segments[-1], value)
+
+    def handle_incoming(self, source_dict, target_obj):
+        self._set_property(
+            target_obj,
+            self.serialize(source_dict[self.json_property])
+        )
+
+    def handle_outgoing(self, source_obj, target_dict):
+        target_dict[self.json_property] = self.deserialize(
+            self._get_property(source_obj)
+        )
+
+    def serialize(self, value):
+        return value
+
+    def deserialize(self, str):
+        return self.type(str)
+
+    def prepare(self, queryset):
+        segments = self.property.split('.')
+        for segment in segments[:-1]:
+            queryset = queryset.select_related(segment)
+        return queryset
--- savory_pie/tests/fields.py
+++ savory_pie/tests/fields.py
@@ -1,7 +1,8 @@
 import unittest
 from mock import Mock
 
-from savory_pie.fields import PropertyField
+from savory_pie.fields import PropertyField, FKPropertyField
+
 
 class PropertyFieldTest(unittest.TestCase):
     def test_simple_outgoing(self):
@@ -28,5 +29,51 @@ class PropertyFieldTest(unittest.TestCase):
         field.handle_incoming(source_dict, target_object)
 
         self.assertEqual(target_object.foo, 20)
+
+    # TODO: test alternate names
+
+
+class FKPropertyFieldTest(unittest.TestCase):
+    def test_simple_outgoing(self):
+        source_object = Mock()
+        source_object.foo.bar = 20
+
+        field = FKPropertyField(property='foo.bar', type=int)
+
+        target_dict = dict()
+
+        field.handle_outgoing(source_object, target_dict)
+
+        self.assertEqual(target_dict['bar'], 20)
+
+    def test_simple_incoming(self):
+        source_dict = {
+            'bar': 20
+        }
+
+        field = FKPropertyField(property='foo.bar', type=int)
+
+        target_object = Mock()
+
+        field.handle_incoming(source_dict, target_object)
+
+        self.assertEqual(target_object.foo.bar, 20)
+
+    def test_prepare(self):
+        query_set = Mock()
+
+        field = FKPropertyField(property='foo.bar.baz', type=int)
+
+        result_query_set = field.prepare(query_set)
+
+        query_set.select_related.assert_called_with('foo')
+
+        query_set_1 = query_set.select_related.return_value
+        query_set_1.select_related.assert_called_with('bar')
+
+        query_set_2 = query_set_1.select_related.return_value
+        self.assertFalse(query_set_2.select_related.called)
+
+        self.assertEqual(query_set_2, result_query_set)
 
     # TODO: test alternate names
