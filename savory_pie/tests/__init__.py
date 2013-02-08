25a557f8f145140fd3d4ba492a13289f887eedfc
dhawkins@ruelala.com <dhawkins@ruelala.com>
2013-02-08 14:02:41 -0500

    Adding tests for the views

--- savory_pie/tests/__init__.py
+++ savory_pie/tests/__init__.py
@@ -1,2 +1,3 @@
 from fields import *
 from resources import *
+from views import *
--- savory_pie/tests/views.py
+++ savory_pie/tests/views.py
@@ -1,1 +1,52 @@
+import unittest
+from StringIO import StringIO
 
+from mock import Mock
+from savory_pie import views
+
+class Request(object):
+    def __init__(self, method, path, body=None, GET=None, POST=None):
+        self.method = method
+        self.body = body
+        self.body_file = None
+
+        self.GET = GET or {}
+        self.POST = POST or {}
+        self.REQUEST = dict(self.GET, **self.POST)
+
+    def read(self):
+        if not self.body_file:
+            self.body_file = StringIO(self.body)
+
+        return self.body_file.read()
+
+
+class ViewTest(unittest.TestCase):
+    def test_get_success(self):
+        resource = Mock()
+        resource.get = Mock(return_value={})
+
+        view = views.service_dispatcher(resource)
+        view(Request(method='GET', path='/'))
+
+        self.assertTrue(resource.get.called)
+
+    def test_put_success(self):
+        root_resource = Mock()
+        new_resource = Mock()
+
+        root_resource.put = Mock(return_value=new_resource)
+
+        view = views.service_dispatcher(root_resource)
+        view(Request(method='PUT', path='/', body='{}'))
+
+        root_resource.put.assert_called_with({})
+
+    def test_post_success(self):
+        root_resource = Mock()
+        root_resource.post = Mock()
+
+        view = views.service_dispatcher(root_resource)
+        view(Request(method='POST', path='/', body='{}'))
+
+        root_resource.post.assert_called_with({})
