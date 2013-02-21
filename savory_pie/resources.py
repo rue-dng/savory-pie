af2b599d1c6c6240cfc5c67fff4825f8f51dfd21
dhawkins@ruelala.com <dhawkins@ruelala.com>
2013-02-21 10:07:21 -0500

    Header doc for Related

--- savory_pie/resources.py
+++ savory_pie/resources.py
@@ -116,6 +116,14 @@ class Related(object):
             return self._prefix + '__' + attribute
 
     def select(self, attribute):
+        """
+        Called to select a related attribute -- this typically translates to a
+        select_related call on the final queryset.
+
+        When select is called on a sub-Related created directly or indirectly
+        through a sub_prefetch, select-s will automatically be translated into
+        prefetch-es.
+        """
         # If a select call is made on a Related that was created through sub_prefetch,
         # that call must be converted into prefetch because the relationship to the
         # top element will not have a cardinality of 1.
@@ -126,10 +134,22 @@ class Related(object):
         return self
 
     def prefetch(self, attribute):
+        """
+        Called to prefetch a related attribute -- this translates into a
+        prefetch_related call on the final queryset.
+        """
         self._prefetch.add(self.translate(attribute))
         return self
 
     def sub_select(self, attribute):
+        """
+        Creates a sub-Related through this relationship.  All calls to select or
+        prefetch on the resulting sub-Related will be automatically qualified with
+        {attribute}__.
+
+        A sub-select Related acquired through a sub-prefetch Related will continue
+        to translates all select-s to prefetch-es.
+        """
         return Related(
             prefix=self.translate(attribute),
             select=self._select,
@@ -138,6 +158,14 @@ class Related(object):
         )
 
     def sub_prefetch(self, attribute):
+        """
+        Creates a sub-Related through this relationship.  All calls to select or
+        prefetch on the resulting sub-Related will be automatically qualified with
+        {attribute}__.
+
+        Furthermore, all select-s on the sub-related will be translated into
+        prefetch-es because they will be read indirectly through a many relationship.
+        """
         return Related(
             prefix=self.translate(attribute),
             select=self._select,
@@ -146,6 +174,10 @@ class Related(object):
         )
 
     def prepare(self, queryset):
+        """
+        Should be called after all select and prefetch calls have been made to
+        applied the accumulated confiugration to a QuerySet.
+        """
         if self._select:
             queryset = queryset.select_related(*self._select)
 
