2286801c9499596fa1da5a10a8212e03216241c4
Jason Michalski <jmichalski@ruelala.com>
2013-02-22 10:38:14 -0500

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='savory-pie',
+    url='https://github.com/RueLaLa/savory_pie',
+    version='0.1',
+    description='A RESTful api libary with support for django',
+    packages=find_packages(),
+    install_requires=[],
+)
