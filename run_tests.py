3e572bbf051d35bfaea66901f14ff4aa00c50388
Jason Michalski <jmichalski@ruelala.com>
2013-02-22 13:08:10 -0500

    run_tests should not depend on your cwd

--- run_tests.py
+++ run_tests.py
@@ -3,7 +3,9 @@ import sys
 import os
 import subprocess
 
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
 os.environ['DJANGO_SETTINGS_MODULE'] = 'savory_pie.tests.dummy_settings'
-args = ['nosetests']
+args = ['nosetests', '-w', PROJECT_ROOT]
 args.extend(sys.argv[1:])
 subprocess.call(args)
