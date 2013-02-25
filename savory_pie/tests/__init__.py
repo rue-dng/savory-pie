65a380dd04e1912f9b9e5e583a319d452574f6f6
Jason Michalski <jmichalski@ruelala.com>
2013-02-25 10:36:01 -0500

    We will now convert any unknown type to str in JSON

--- savory_pie/tests/__init__.py
+++ savory_pie/tests/__init__.py
@@ -3,3 +3,4 @@ from savory_pie.tests.fields import *
 from savory_pie.tests.resources import *
 from savory_pie.tests.views import *
 from savory_pie.tests.integration import *
+from savory_pie.tests.formaters import *
