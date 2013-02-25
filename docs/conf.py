f76888b3c47e3668d3d1ea0b0d05cc841f44603a
Jason Michalski <armooo@armooo.net>
2013-02-24 21:37:00 -0500

    Rue -> Rue La La

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Savory Pie'
-copyright = u'2013, Rue'
+copyright = u'2013, Rue La La'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -91,7 +91,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -184,7 +184,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'SavoryPie.tex', u'Savory Pie Documentation',
-   u'Rue', 'manual'),
+   u'Rue La La', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -214,7 +214,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'savorypie', u'Savory Pie Documentation',
-     [u'Rue'], 1)
+     [u'Rue La La'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -228,7 +228,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'SavoryPie', u'Savory Pie Documentation',
-   u'Rue', 'SavoryPie', 'One line description of project.',
+   u'Rue La La', 'SavoryPie', 'One line description of project.',
    'Miscellaneous'),
 ]
 
