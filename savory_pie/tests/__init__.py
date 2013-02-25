dd8707654c3fe5bec515f354da338236f9b99687
Jason Michalski <jmichalski@ruelala.com>
2013-02-25 10:44:32 -0500

    Move unittests around so we do not need to update init

--- savory_pie/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-from savory_pie.tests.django_utils import *
-from savory_pie.tests.fields import *
-from savory_pie.tests.resources import *
-from savory_pie.tests.views import *
-from savory_pie.tests.integration import *
-from savory_pie.tests.formaters import *
