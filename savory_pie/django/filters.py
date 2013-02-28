f14e0a3dbd3b2a3e648f5d295109d05e425b9f64
dhawkins@ruelala.com <dhawkins@ruelala.com>
2013-02-28 14:19:54 -0500

    Sketch of what a filter API might look

--- savory_pie/django/filters.py
+++ savory_pie/django/filters.py
@@ -2,6 +2,24 @@
 #   def filter(self, ctx, params, queryset)
 #   def describe(self, ctx, schema_dict)
 
-
 class StandardFilter(object):
-    def __init__(self, fields, order_by=None)
+    def __init__(self, fields, order_by=None):
+        pass
+
+    def filter_by(self, attribute, type, ops, published_property=None):
+        pass
+
+    def order_by(self, attribute, published_property=None):
+        pass
+
+    def default_order_by(self, attribute, desc=False):
+        pass
+
+    def filter(self, ctx, params, queryset):
+        pass
+
+    def describe(self, ctx, schema_dict):
+        pass
+
+
+filter = StandardFilter().filter_by('first_name', 'last_name', type=str, ops=)
