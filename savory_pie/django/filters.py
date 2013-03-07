2c2eeeb2fc2075c956f70123e298d5747290f184
Will Ware <wware@ruelala.com>
2013-03-07 15:51:23 -0500

    preliminary work on implementing filters in SavoryPie

--- savory_pie/django/filters.py
+++ savory_pie/django/filters.py
@@ -1,30 +1,33 @@
 #protocol QuerySetFilter:
 #   def filter(self, ctx, params, queryset)
-    """
-    Filters (or orders) the queryset according to the specified criteria
-    """
+#       """
+#       Filters (or orders) the queryset according to the specified criteria
+#       """
 #   def describe(self, ctx, schema_dict)
-    """
-    Fills in schema_dict with information about the set of valid filtering
-    criteria.
-    """
+#       """
+#       Fills in schema_dict with information about the set of valid filtering
+#       criteria.
+#       """
 
 class StandardFilter(object):
+
     def __init__(self, fields, order_by=None):
-        pass
+        self.fields = fields
+        self._order_by = order_by
 
     def filter_by(self, attribute, type, ops, published_property=None):
-        pass
+        pass   # TODO
 
     def order_by(self, attribute, published_property=None):
-        pass
+        pass   # TODO
 
     def default_order_by(self, attribute, desc=False):
-        pass
+        pass   # TODO
 
     def filter(self, ctx, params, queryset):
-        pass
+        # ignore ctx and params
+        return queryset.filter(**self.fields)
 
     def describe(self, ctx, schema_dict):
-        pass
+        schema_dict.update(self.fields)
 
