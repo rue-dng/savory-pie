d9bb0eba6f2365f067cb2a8c80f8f4f848c99b5b
Will Ware <wware@ruelala.com>
2013-03-11 16:44:31 -0400

    Allow filters to take parameters from querystrings.
    This would be useful for something like a filter to select a single
    user by name or email, or for selecting all products whose price
    falls below or above some threshold provided in the querystring.

--- savory_pie/django/filters.py
+++ savory_pie/django/filters.py
@@ -11,20 +11,34 @@
 
 class StandardFilter(object):
 
-    def __init__(self, name, fields, order_by=None):
+    def __init__(self, name, criteria, order_by=None):
         self.name = name
-        self.fields = fields
+        self.paramkey = None
+        self.criteria = criteria
         self._order_by = [] if order_by is None else order_by
 
     def __repr__(self):
         return '<' + self.__class__.__name__ + ': ' + self.name + '>'
 
     def filter(self, ctx, params, queryset):
-        if self.name in params._GET.getlist('filter'):
-            queryset = queryset.filter(**self.fields)
+        if params._GET.__contains__(self.name):
+            criteria = self.criteria.copy()
+            if self.paramkey is not None:
+                criteria[self.paramkey] = params._GET.get(self.name)
+            queryset = queryset.filter(**criteria)
             if self._order_by is not None:
                 queryset = queryset.order_by(*self._order_by)
         return queryset
 
     def describe(self, ctx, schema_dict):
-        schema_dict.update(self.fields)
+        schema_dict['filtering'] = self.criteria
+        schema_dict['ordering'] = self._order_by
+
+
+class ParameterizedFilter(StandardFilter):
+
+    def __init__(self, name, paramkey, criteria={}, order_by=None):
+        self.name = name
+        self.paramkey = paramkey
+        self.criteria = criteria
+        self._order_by = [] if order_by is None else order_by
