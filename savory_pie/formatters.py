d9b7d25f3d0927e38a1bc5ac3f1a6f7f1371f2f6
David Fox <dave@cantinaconsulting.com>
2013-03-14 14:47:33 -0400

    Made date format the date with timezone

--- savory_pie/formatters.py
+++ savory_pie/formatters.py
@@ -41,7 +41,7 @@ class JSONFormatter(object):
     # Not 100% happy with this API review pre 1.0
     def to_api_value(self, type_, python_value):
         if issubclass(type_, datetime.datetime):
-            return python_value.strftime("%Y-%m-%dT%H:%M:%S")
+            return python_value.isoformat("T")
         elif type(python_value) not in (int, long, float, dict, list,
                                         bool, str, unicode, type(None)):
             return str(python_value)
