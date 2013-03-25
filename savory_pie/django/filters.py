52ae6208be7d09625fd32bbae1e3bdcdc417cca6
Will Ware <wware@ruelala.com>
2013-03-25 15:48:23 -0400

    Parameterized filters must handle various arguments.

--- savory_pie/django/filters.py
+++ savory_pie/django/filters.py
@@ -1,3 +1,7 @@
+import datetime
+import re
+import string
+
 #protocol QuerySetFilter:
 #   def filter(self, ctx, params, queryset)
 #       """
@@ -11,24 +15,34 @@
 
 class StandardFilter(object):
 
+    dateRegex = re.compile('(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})')
+
     def __init__(self, name, criteria, order_by=None):
         self.name = name
-        self.paramkey = None
-        self.criteria = criteria
-        self._order_by = [] if order_by is None else order_by
+        self.criteria = criteria or {}
+        self._order_by = order_by or []
 
     def __repr__(self):
         return '<' + self.__class__.__name__ + ': ' + self.name + '>'
+
+    def get_param_value(self, name, params, criteria):
+        pass
 
     def filter(self, ctx, params, queryset):
         name = ctx.formatter.convert_to_public_property(self.name)
         if name in params._GET:
             criteria = self.criteria.copy()
-            if self.paramkey is not None:
-                criteria[self.paramkey] = params._GET.get(name)
+            self.get_param_value(name, params, criteria)
+            limit = None
+            # Django just uses 'limit' for this but there might be legitimate uses
+            # in models for a field called 'limit', so use a more specific name.
+            if criteria.has_key('limit_object_count'):
+                limit = criteria.pop('limit_object_count')
             queryset = queryset.filter(**criteria)
             if self._order_by is not None:
                 queryset = queryset.order_by(*self._order_by)
+            if limit is not None:
+                queryset = queryset[:limit]
         return queryset
 
     def describe(self, ctx, schema_dict):
@@ -43,3 +57,20 @@ class ParameterizedFilter(StandardFilter):
         self.paramkey = paramkey
         self.criteria = criteria or {}
         self._order_by = order_by or []
+
+    def get_param_value(self, name, params, criteria):
+        value = params._GET.get(name)
+        m = self.dateRegex.match(value)
+        if m is not None:
+            year, month, date, hour, minute, second = \
+                map(string.atoi, [m.group(i) for i in range(1, 7)])
+            value = datetime.datetime(year, month, date, hour, minute, second)
+        else:
+            try:
+                value = float(value)
+            except ValueError:
+                try:
+                    value = int(value)
+                except ValueError:
+                    pass
+        criteria[self.paramkey] = value
