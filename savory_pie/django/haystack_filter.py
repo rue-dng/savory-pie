6081dbaab6cd4efd372cfdf38cca53a116e751d9
Will Ware <wware@ruelala.com>
2013-04-12 10:29:41 -0400

    Avoid extra MySQL hit in for-loop.

--- savory_pie/django/haystack_filter.py
+++ savory_pie/django/haystack_filter.py
@@ -9,19 +9,15 @@ class HaystackFilter(ParameterizedFilter):
     Any additional search criteria, or the order_by stuff, is subsequently treated normally.
     """
 
-    def __init__(self, model, name='q', criteria=None, order_by=None):
+    def __init__(self, name='q', criteria=None, order_by=None):
         ParameterizedFilter.__init__(self, name, name, criteria, order_by)
-        self.model = model
-
-    def make_iter(self, qs):
-        for result in qs:
-            yield result.object.pk
 
     def filter(self, ctx, params, queryset):
         name = self.is_applicable(ctx, params)
         if name:
             value = self.get_param_value(name, ctx, params)
-            pks = self.make_iter(SearchQuerySet().filter(content=str(value)))
-            queryset = self.model.objects.filter(pk__in=pks)
+            hs_results = SearchQuerySet().filter(content=unicode(value)).models(queryset.model)
+            pks = (result.pk for result in hs_results)
+            queryset = queryset.filter(pk__in=pks)
             queryset = self.apply(self.criteria, queryset)
         return queryset
