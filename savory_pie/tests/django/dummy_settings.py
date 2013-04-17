664b1ac743a4b9c88b0ddd62c020a16f95799605
Jason Michalski <jmichalski@ruelala.com>
2013-04-17 10:55:39 -0400

    Add HAYSTACK_SEARCH_ENGINE back to the settings

--- savory_pie/tests/django/dummy_settings.py
+++ savory_pie/tests/django/dummy_settings.py
@@ -8,6 +8,7 @@ PROJECT_ROOT = os.path.join(os.path.dirname(__file__))
 TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, 'templates'),)
 
 HAYSTACK_SITECONF = 'savory_pie.tests.django.search_conf'
+HAYSTACK_SEARCH_ENGINE = 'dummy'
 
 DATABASES = {
     'default': {
