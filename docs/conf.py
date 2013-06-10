bc67d49840ffbc3902a7e34298d6aee4a29bceaa
Jason Michalski <armooo@armooo.net>
2013-06-09 20:18:14 -0400

    flake8 doc files

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,8 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import sys
+import os
 
 # Load the django settings
 os.environ['DJANGO_SETTINGS_MODULE'] = 'savory_pie.tests.django.dummy_settings'
@@ -173,22 +174,25 @@ htmlhelp_basename = 'SavoryPiedoc'
 # -- Options for LaTeX output --------------------------------------------------
 
 latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+    # The paper size ('letterpaper' or 'a4paper').
+    #'papersize': 'letterpaper',
 
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+    # The font size ('10pt', '11pt' or '12pt').
+    #'pointsize': '10pt',
 
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
+    # Additional stuff for the LaTeX preamble.
+    #'preamble': '',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
-latex_documents = [
-  ('index', 'SavoryPie.tex', u'Savory Pie Documentation',
-   u'Rue La La', 'manual'),
-]
+latex_documents = [(
+    'index',
+    'SavoryPie.tex',
+    u'Savory Pie Documentation',
+    u'Rue La La',
+    'manual'
+), ]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
@@ -229,11 +233,15 @@ man_pages = [
 # Grouping the document tree into Texinfo files. List of tuples
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
-texinfo_documents = [
-  ('index', 'SavoryPie', u'Savory Pie Documentation',
-   u'Rue La La', 'SavoryPie', 'One line description of project.',
-   'Miscellaneous'),
-]
+texinfo_documents = [(
+    'index',
+    'SavoryPie',
+    u'Savory Pie Documentation',
+    u'Rue La La',
+    'SavoryPie',
+    'One line description of project.',
+    'Miscellaneous'
+), ]
 
 # Documents to append as an appendix to all manuals.
 #texinfo_appendices = []
