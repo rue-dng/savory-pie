e186803a9f8345a7f2f673d4d8af47936b7303cf
Geoffrey Clements <gclements@ruelala.com>
2013-06-12 09:41:27 -0400

    flake8

--- savory_pie/django/utils.py
+++ savory_pie/django/utils.py
@@ -8,6 +8,7 @@ def getLogger():
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
     logger.addHandler(handler)
+
     def logger_pprint(obj, logger=logger):
         if logger.isEnabledFor(logging.DEBUG):
             logger._log(logging.DEBUG, '\n' + pprint.pformat(obj), [], {})
