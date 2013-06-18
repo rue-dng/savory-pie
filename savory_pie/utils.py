c5a8887681f7abf6582c7312a62490914e61171f
aliu <aliu@ruelala.com>
2013-06-18 14:14:44 -0400

    Add a ParamsDictImpl class to wrap dict to a list

--- savory_pie/utils.py
+++ savory_pie/utils.py
@@ -25,3 +25,44 @@ def to_list(items):
     except:
         pass
     return items
+
+
+class _ParamsDictImpl(object):
+    """
+    Simple class that wraps a dictionary and returns a list, this is used since filters
+    expects a list.
+
+        Parameters:
+
+            ``params``
+                This is a dictionary of params
+    """
+    def __init__(self, params):
+        self._params = params
+
+    def keys(self):
+        return self._params.keys()
+
+    def __contains__(self, key):
+        return key in self._params
+
+    def __getitem__(self, key):
+        return self._params.get(key, None)
+
+    def get(self, key, default=None):
+        return self._params.get(key, default)
+
+    def get_as(self, key, type, default=None):
+        value = self._params.get(key, None)
+        return default if value is None else type(value)
+
+    def get_list(self, key):
+        return [self._params[key]]
+
+    def get_list_of(self, key, type):
+        list = self._params.get(key, None)
+        if list is None:
+            return []
+        else:
+            return [type(x) for x in list]
+
