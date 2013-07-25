c4e48c8ffcb4d1fd517d71a23a4002ba6ba4db73
Will Ware <wware@ruelala.com>
2013-07-25 17:49:24 -0400

    Tests for HaystackField and HaystackSearchResource

--- savory_pie/tests/django/test_haystack.py
+++ savory_pie/tests/django/test_haystack.py
@@ -1,10 +1,15 @@
 import unittest
 
 import mock
+import haystack.inputs
+
 from django.db import models
 
-from savory_pie.tests.mock_context import mock_context
+from savory_pie.formatters import JSONFormatter
 from savory_pie.django.haystack_filter import HaystackFilter
+from savory_pie.django.resources import HaystackSearchResource
+from savory_pie.django.fields import HaystackField
+from savory_pie.tests.mock_context import mock_context
 from test_filters import Params
 
 
@@ -74,3 +79,41 @@ class HaystackFilterTest(unittest.TestCase):
             ],
             any_order=True
         )
+
+
+class HaystackSearchResourceTest(unittest.TestCase):
+
+    @mock.patch('__builtin__.enumerate')
+    @mock.patch('savory_pie.django.resources.SearchQuerySet')
+    def test_haystack_search_resource(self, haystack_qs_cls, enum_mock):
+        n = 3
+        haystack_qs_cls.return_value = haystack_qs = mock.Mock()
+        haystack_qs.filter.return_value = haystack_qs
+        haystack_qs.models.return_value = haystack_qs
+        haystack_qs.count.return_value = n
+
+        enum_mock.get_stored_fields.return_value = enum_mock
+        enum_mock.__getitem__.return_value = '"api-as-string"'
+        enum_mock.return_value = [(i, enum_mock) for i in range(n)]
+
+        sr = HaystackSearchResource()
+        sr.model_class = TestModel
+        ctx = mock.Mock()
+        self.assertEqual(
+            ''.join([x for x in sr.get(ctx, {'q': 'foo'})]),
+            '{"meta":{"count":3},"objects":["api-as-string","api-as-string","api-as-string"]}')
+        haystack_qs.models.assert_called_with(TestModel)
+        haystack_qs.count.assert_called_with()
+
+
+class HaystackFieldTest(unittest.TestCase):
+
+    def test_haystack_field(self):
+        FooResource = mock.Mock()
+        FooResource.return_value = frv =  mock.Mock()
+        frv.get.return_value = x = {'a': 'b'}
+        api = HaystackField(
+            base_uri='/my/api/path/',
+            formatter=JSONFormatter(),
+            resource=FooResource)
+        self.assertEqual(api.prepare(None), '{"a": "b", "$stale": true}')
