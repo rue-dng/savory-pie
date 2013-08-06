66dc1785f519e0fce982aa0d764325496018f2bf
Jason Michalski <jmichalski@ruelala.com>
2013-08-06 19:21:00 -0400

    Make indexing faster

--- savory_pie/django/haystack_field.py
+++ savory_pie/django/haystack_field.py
@@ -1,11 +1,37 @@
 import json
 import logging
 
-from savory_pie.context import APIContext
-
+from haystack import indexes
 from haystack import fields as haystack_fields
 
+from savory_pie.context import APIContext
+from savory_pie.django.utils import Related
+
+
 logger = logging.getLogger(__name__)
+
+
+class ResourceIndex(indexes.SearchIndex):
+
+    def prefetch_related(self, related):
+        pass
+
+    def get_model(self):
+        return self.resource_class.model_class
+
+    def _prefetch_related(self, qs):
+        related = Related()
+        ctx = APIContext('', None, None)
+        self.resource_class.prepare(ctx, related)
+        self.prefetch_related(related)
+        return related.prepare(qs)
+
+    def index_queryset(self, using=None):
+        qs = self.get_model().objects.all()
+        return self._prefetch_related(qs)
+
+    def build_queryset(self, start_date=None, end_date=None, using=None):
+        return self.index_queryset(using=using)
 
 
 class HaystackField(haystack_fields.CharField):
