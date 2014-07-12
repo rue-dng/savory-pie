71fc4e720b841508033f8fa71c49c75eb46b813b
Jason Michalski <armooo@armooo.net>
2014-07-11 22:58:53 -0700

    Add basic travisci config

--- run_tests.py
+++ run_tests.py
@@ -8,4 +8,4 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'savory_pie.tests.django.dummy_settings'
 args = ['nosetests', '-s', '-w', PROJECT_ROOT]
 args.extend(sys.argv[1:])
-subprocess.call(args)
+sys.exit(subprocess.call(args))
