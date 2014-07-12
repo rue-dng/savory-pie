a1f716f9a8648af8f01ed9e1cb9e328413841e31
Mike Milkin <mmilkin@gmail.com>
2014-07-11 23:49:38 -0400

    fixing setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,28 @@
 from setuptools import setup
 from setuptools import find_packages
 
+version = '0.1'
+
 setup(
     name='savory-pie',
+    version=version,
     url='https://github.com/RueLaLa/savory_pie',
-    version='0.1',
+    version=version,
     description='A RESTful api libary with support for django',
+    license='MIT',
     packages=find_packages(),
     install_requires=[],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
