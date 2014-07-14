cee45fbd050cab8012cb4c864907d220b529c041
Mike Milkin <mmilkin@gmail.com>
2014-07-13 21:40:41 -0400

    Adding missing tests

--- savory_pie/tests/django/test_utils.py
+++ savory_pie/tests/django/test_utils.py
@@ -1,6 +1,146 @@
+import logging
 import unittest
-from savory_pie.django.utils import Related
+import mock
+from savory_pie.django.utils import Related, getLogger
 from savory_pie.tests.django import mock_orm
+
+class LoggerTestCase(unittest.TestCase):
+
+    def test_logger_callable(self):
+        with self.assertRaises(AssertionError):
+            logger = getLogger()
+            logger.callable('some var', logger=None)
+
+    def test_logger_im_func(self):
+        func = mock.MagicMock(return_value=None, name='logger')
+
+        im_func = mock.MagicMock(name='im_func')
+
+        func_code = mock.MagicMock(name='func_code')
+        im_func.func_code = func_code
+        func.im_func = im_func
+
+        func_code.co_filename = 'file name'
+        func_code.co_firstlineno = 23
+        func_code.co_name = 'name'
+
+        logger = mock.MagicMock()
+
+        lg = getLogger()
+        lg.callable(func, logger=logger)
+        logger._log.assert_called_with(logging.DEBUG, 'file name:23 name', [], {})
+
+    def test_logger(self):
+        func = mock.MagicMock(return_value=None, name='logger', spec=['func_code'])
+
+        func_code = mock.MagicMock(name='func_code')
+        func.func_code = func_code
+
+        func_code.co_filename = 'file name'
+        func_code.co_firstlineno = 23
+        func_code.co_name = 'name'
+
+        logger = mock.MagicMock()
+
+        lg = getLogger()
+        lg.callable(func, logger=logger)
+        logger._log.assert_called_with(logging.DEBUG, 'file name:23 name', [], {})
+
+
+    def test_logger_alert(self):
+        logger = mock.MagicMock()
+
+        lg = getLogger()
+        lg.alert('object', marker='1', logger=logger)
+        bannerEdge = '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1'
+
+        calls = [
+            mock.call(logging.DEBUG, bannerEdge, [], {}),
+            mock.call(logging.DEBUG, bannerEdge, [], {}),
+            mock.call(logging.DEBUG, '                                     object', [], {}),
+            mock.call(logging.DEBUG, bannerEdge, [], {}),
+            mock.call(logging.DEBUG, bannerEdge, [], {}),
+
+        ]
+        logger._log.assert_has_calls(calls)
+
+    @mock.patch('savory_pie.django.utils.pprint.pformat')
+    def test_pp_enabled(self, pformat):
+        pformat.return_value = 'something pprinted'
+        logger = mock.MagicMock()
+        logger.isEnabledFor.return_value = True
+        lg = getLogger()
+        lg.pprint('object', logger=logger)
+        logger._log.assert_called_with(logging.DEBUG, '\nsomething pprinted', [], {})
+
+    @mock.patch('savory_pie.django.utils.pprint.pformat')
+    def test_pp_dis_enabled(self, pformat):
+        pformat.return_value = 'something pprinted'
+        logger = mock.MagicMock()
+        logger.isEnabledFor.return_value = False
+        lg = getLogger()
+        lg.pprint('object', logger=logger)
+        self.assertFalse(logger._log.called)
+
+    def test_logger_before_disabled(self):
+        logger = mock.MagicMock()
+        logger.isEnabledFor.return_value = False
+        lg = getLogger()
+        lg.before_queries(txt='object', logger=logger)
+        self.assertFalse(logger._log.called)
+
+    @mock.patch('savory_pie.django.utils.connection')
+    def test_logger_before_queries(self, connection):
+        connection.queries = [1,2]
+        logger = mock.MagicMock()
+        logger.isEnabledFor.return_value = True
+        lg = getLogger()
+        lg.before_queries(txt='object', logger=logger)
+        logger._log.assert_called_with(logging.DEBUG, 'object', [], {})
+        self.assertEqual(logger._num_queries, 2)
+
+    @mock.patch('savory_pie.django.utils.connection')
+    def test_logger_before_queries_notxt(self, connection):
+        connection.queries = [1, 2]
+        logger = mock.MagicMock()
+        logger.isEnabledFor.return_value = True
+        lg = getLogger()
+        lg.before_queries(logger=logger)
+        self.assertFalse(logger._log.called)
+        self.assertEqual(logger._num_queries, 2)
+
+    @mock.patch('savory_pie.django.utils.connection')
+    def test_logger_before_queries_disabled(self, connection):
+        connection.queries = [1, 2]
+        logger = mock.MagicMock()
+        logger._num_queries = 0
+        logger.isEnabledFor.return_value = False
+        lg = getLogger()
+        lg.after_queries(logger=logger)
+        self.assertFalse(logger._log.called)
+        self.assertEqual(logger._num_queries, 0)
+
+
+    @mock.patch('savory_pie.django.utils.connection')
+    def test_logger_after_queries_enabled(self, connection):
+        logger = mock.MagicMock()
+        logger._num_queries = 1
+        qry1 = {'name': 'qry1', 'time': '1', 'sql': 'select'}
+        qry2 = {'name': 'qry2', 'time': '2', 'sql': 'insert'}
+        qry3 = {'name': 'qry3', 'time': '3', 'sql': 'update'}
+
+        connection.queries = [qry1, qry2, qry3]
+        logger.isEnabledFor.return_value = True
+
+        lg = getLogger()
+        lg.after_queries(obj='object', logger=logger)
+
+        calls = [
+            mock.call(logging.DEBUG, 'Database hits: 2', [], {}),
+            mock.call(logging.DEBUG, 'Database hit, 2 seconds\ninsert', [], {}),
+            mock.call(logging.DEBUG, 'Database hit, 3 seconds\nupdate', [], {}),
+        ]
+        logger._log.assert_has_calls(calls)
 
 
 class RelatedTest(unittest.TestCase):
